0bec60d2103ccd9b3ac2c3d7a85c63c8a32a7058
Bastian Koppelmann <kbastian@mail.uni-paderborn.de>
2020-10-05 15:59:09 +0200

    Enable GPIO as LED for WDT mux
    Signed-off-by: Bastian Koppelmann <kbastian@mail.uni-paderborn.de>

--- software/led-bmi160-demonstrator/watchdog.c
+++ software/led-bmi160-demonstrator/watchdog.c
@@ -1,4 +1,5 @@
 #include "watchdog.h"
+#include "platform.h"
 
 volatile WDTUnit *wdt_array = (WDTUnit*)0x10000200;
 volatile WDTElement *wdt = (WDTElement*)0x10000208;
@@ -8,6 +9,11 @@ const uint32_t WDT_KEY = 0x51F15E;
 
 const uint8_t WDT_SYSTEM_RST = (1 << 0);
 const uint8_t WDT_LED        = (1 << 1);
+
+enum {
+  WDT_MODE_TIMEOUT = 0,
+  WDT_MODE_WINDOW = 1,
+};
 
 static void wdt_unlock(void)
 {
@@ -35,29 +41,34 @@ void wdt_disable(void)
 void wdt_configure(uint64_t counter_start, uint32_t counter_len,
                    uint16_t err_pulse_len)
 {
-    WDTConfig tmpconf;
-    tmpconf.cfg = 0;
+  GPIO_REG(GPIO_IOF_SEL) &= ~(1 << 19);
+  GPIO_REG(GPIO_IOF_EN) |= (1 << 19);
+  WDTConfig tmpconf;
+  tmpconf.cfg = 0;
 
-    tmpconf.field.scale = 0; // no scaling
-    tmpconf.field.zerocmp = 1; // flush counter to zero after reset
-    tmpconf.field.mode = 1; // deglitch
-    tmpconf.field.interrupt =  0; // no interrupt pending
-    tmpconf.field.outputen = 0; // no global rst on exception
+  tmpconf.field.scale = 0;               // no scaling
+  tmpconf.field.zerocmp = 1;             // flush counter to zero after reset
+  tmpconf.field.mode = WDT_MODE_TIMEOUT; // deglitch
+  tmpconf.field.interrupt = 0;           // no interrupt pending
+  tmpconf.field.outputen = 1;            // no global rst on exception
 
-    wdt_unlock();
-    wdt->cfg = tmpconf;
+  wdt_unlock();
+  wdt->cfg = tmpconf;
 
-    wdt_unlock();
-    wdt->count = counter_start;
+  wdt_unlock();
+  wdt->feed = WDT_FOOD;
 
-    wdt_unlock();
-    wdt->compare[0] = counter_len;
+  wdt_unlock();
+  wdt->count = counter_start;
 
-    wdt_unlock();
-    wdt->pulsewidth = err_pulse_len;
+  wdt_unlock();
+  wdt->compare[0] = counter_len;
 
-    wdt_unlock();
-    wdt->mux = WDT_LED;
+  wdt_unlock();
+  wdt->pulsewidth = err_pulse_len;
+
+  wdt_unlock();
+  wdt->mux = WDT_LED;
 }
 
 void wdt_enable(void)
@@ -71,5 +82,3 @@ void wdt_pet(void)
     wdt_unlock();
     wdt->feed = WDT_FOOD;
 }
-
-
